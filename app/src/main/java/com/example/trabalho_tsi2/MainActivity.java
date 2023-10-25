e2af7372290d4f48e072af647ac59c70d9f731c9
Rodinei Martins Coelho <rodinei.m.coelho@gmail.com>
2023-10-25 17:49:35 -0400

    Add about on initial activity

--- app/src/main/java/com/example/trabalho_tsi2/MainActivity.java
+++ app/src/main/java/com/example/trabalho_tsi2/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.trabalho_tsi2.about.AboutActivity;
 import com.example.trabalho_tsi2.home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button continueButton;
+    private Button aboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.continueButton = findViewById(R.id.continueButton);
+        this.aboutButton = findViewById(R.id.aboutButton);
+
         this.continueButton.setOnClickListener(view -> {
             Intent intent = new Intent(this, HomeActivity.class);
             startActivity(intent);
             finish();
+        });
+
+        this.aboutButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
         });
     }
 }
